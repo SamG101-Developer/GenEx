6cab19657f01ad5b180278afc9701593f33dc451
Sam Gardner <samuelgardner101@gmail.com>
2025-08-07 21:26:48 +0100

    Add missing `&&` rvalue references

--- include/genex/iterators/next.hpp
+++ include/genex/iterators/next.hpp
@@ -13,14 +13,14 @@ namespace genex::iterators {
     concept has_next = has_std_next<I> || has_operator_plusplus<I>;
 
     struct next_fn {
-        template <concepts::iterator I> requires (has_std_next<I>)
-        constexpr auto operator()(I it, const std::size_t n = 1) const noexcept -> decltype(auto) {
-            return std::next(it, n);
+        template <iterator I> requires (has_std_next<I>)
+        constexpr auto operator()(I &&it, const std::size_t n = 1) const noexcept -> decltype(auto) {
+            return std::next(std::forward<I>(it), n);
         }
 
-        template <concepts::iterator I> requires (not has_std_next<I> && has_operator_plusplus<I>)
-        constexpr auto operator()(I it, const std::size_t n = 1) const noexcept -> decltype(auto) {
-            auto copy_it = it;
+        template <iterator I> requires (not has_std_next<I> && has_operator_plusplus<I>)
+        constexpr auto operator()(I &&it, const std::size_t n = 1) const noexcept -> decltype(auto) {
+            auto copy_it = std::forward<I>(it);
             for (std::size_t i = 0; i < n; ++i) { ++copy_it; }
             return copy_it;
         }
