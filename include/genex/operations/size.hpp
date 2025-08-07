1ef5a7520596145d05e6f443378cec719907851e
Sam Gardner <samuelgardner101@gmail.com>
2025-08-07 17:45:47 +0100

    Size specialization for generators

--- include/genex/operations/size.hpp
+++ include/genex/operations/size.hpp
@@ -12,13 +12,22 @@ namespace genex::operations {
 
     struct size_fn {
         template <typename Rng> requires (has_member_size<Rng>)
-        constexpr auto operator()(Rng &&r) const noexcept -> size_t {
+        constexpr auto operator()(Rng &&r) const noexcept -> std::size_t {
             return r.size();
         }
 
         template <typename Rng> requires (not has_member_size<Rng> and has_std_size<Rng>)
-        constexpr auto operator()(Rng &&r) const noexcept -> size_t {
+        constexpr auto operator()(Rng &&r) const noexcept -> std::size_t {
             return std::size(r);
+        }
+
+        template <typename T>
+        constexpr auto operator()(generator<T> &&gen) const noexcept -> std::size_t {
+            auto count = static_cast<std::size_t>(0);
+            for (auto &&_ : std::move(gen)) {
+                ++count;
+            }
+            return count;
         }
     };
 
