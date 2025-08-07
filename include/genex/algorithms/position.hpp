ab8b911fe5db14fa2602ca430b9fc9e243922466
Sam Gardner <samuelgardner101@gmail.com>
2025-08-07 15:39:48 +0100

    Add a default for the position not found (default -1)

--- include/genex/algorithms/position.hpp
+++ include/genex/algorithms/position.hpp
@@ -14,75 +14,80 @@ using namespace genex::concepts;
 using namespace genex::type_traits;
 
 
+namespace genex::algorithms {
+    using pos_t = std::make_signed_t<std::size_t>;
+}
+
+
 template <iterator I, sentinel S, std::invocable<iter_value_t<I>> Proj = genex::meta::identity, std::predicate<std::invoke_result_t<Proj, iter_value_t<I>>> Pred>
-auto do_position(I &&first, S &&last, Pred &&pred, Proj &&proj = {}) -> std::make_signed_t<std::size_t> {
-    std::make_signed_t<std::size_t> index = 0;
+auto do_position(I &&first, S &&last, Pred &&pred, Proj &&proj = {}, const genex::algorithms::pos_t def = -1) -> genex::algorithms::pos_t {
+    genex::algorithms::pos_t index = 0;
     for (; first != last; ++first, ++index) {
         if (std::invoke(std::forward<Pred>(pred), std::invoke(std::forward<Proj>(proj), *first))) {
             return index;
         }
     }
-    return -1;
+    return def;
 }
 
 
 template <range Rng, std::invocable<range_value_t<Rng>> Proj = genex::meta::identity, std::predicate<std::invoke_result_t<Proj, range_value_t<Rng>>> Pred>
-auto do_position(Rng &&rng, Pred &&pred, Proj &&proj = {}) -> std::make_signed_t<std::size_t> {
-    return do_position(genex::iterators::begin(rng), genex::iterators::end(rng), std::forward<Pred>(pred), std::forward<Proj>(proj));
+auto do_position(Rng &&rng, Pred &&pred, Proj &&proj = {}, const genex::algorithms::pos_t def = -1) -> genex::algorithms::pos_t {
+    return do_position(genex::iterators::begin(rng), genex::iterators::end(rng), std::forward<Pred>(pred), std::forward<Proj>(proj), def);
 }
 
 
 template <iterator I, sentinel S, std::invocable<iter_value_t<I>> Proj = genex::meta::identity, std::predicate<std::invoke_result_t<Proj, iter_value_t<I>>> Pred>
-auto do_position_last(I &&first, S &&last, Pred &&pred, Proj &&proj = {}) -> std::make_signed_t<std::size_t> {
-    std::make_signed_t<std::size_t> index = genex::iterators::distance(first, last) - 1;
+auto do_position_last(I &&first, S &&last, Pred &&pred, Proj &&proj = {}, const genex::algorithms::pos_t def = -1) -> genex::algorithms::pos_t {
+    genex::algorithms::pos_t index = genex::iterators::distance(first, last) - 1;
     last = genex::iterators::next(first, index);
     for (; last != first; --last, --index) {
         if (std::invoke(std::forward<Pred>(pred), std::invoke(std::forward<Proj>(proj), *last))) {
             return index;
         }
     }
-    return -1;
+    return def;
 }
 
 
 template <range Rng, std::invocable<range_value_t<Rng>> Proj = genex::meta::identity, std::predicate<std::invoke_result_t<Proj, range_value_t<Rng>>> Pred>
-auto do_position_last(Rng &&rng, Pred &&pred, Proj &&proj = {}) -> std::make_signed_t<std::size_t> {
-    return do_position_last(genex::iterators::begin(rng), genex::iterators::end(rng), std::forward<Pred>(pred), std::forward<Proj>(proj));
+auto do_position_last(Rng &&rng, Pred &&pred, Proj &&proj = {}, const genex::algorithms::pos_t def = -1) -> genex::algorithms::pos_t {
+    return do_position_last(genex::iterators::begin(rng), genex::iterators::end(rng), std::forward<Pred>(pred), std::forward<Proj>(proj), def);
 }
 
 
 namespace genex::algorithms {
     struct position_fn final : detail::algorithm_base {
         template <iterator I, sentinel S, std::invocable<iter_value_t<I>> Proj = meta::identity, std::predicate<std::invoke_result_t<Proj, iter_value_t<I>>> Pred>
-        constexpr auto operator()(I &&first, S &&last, Pred &&pred, Proj &&proj = {}) const -> std::size_t {
-            MAP_TO_IMPL(do_position, first, last, pred, proj);
+        constexpr auto operator()(I &&first, S &&last, Pred &&pred, Proj &&proj = {}, pos_t def = -1) const -> std::size_t {
+            MAP_TO_IMPL(do_position, first, last, pred, proj, def);
         }
 
         template <range Rng, std::invocable<range_value_t<Rng>> Proj = meta::identity, std::predicate<std::invoke_result_t<Proj, range_value_t<Rng>>> Pred>
-        constexpr auto operator()(Rng &&rng, Pred &&pred, Proj &&proj = {}) const -> std::size_t {
-            MAP_TO_IMPL(do_position, rng, pred, proj);
+        constexpr auto operator()(Rng &&rng, Pred &&pred, Proj &&proj = {}, pos_t def = -1) const -> std::size_t {
+            MAP_TO_IMPL(do_position, rng, pred, proj, def);
         }
 
         template <typename Pred, typename Proj = meta::identity>
-        constexpr auto operator()(Pred &&pred, Proj &&proj = {}) const -> decltype(auto) {
-            MAP_TO_BASE(pred, proj);
+        constexpr auto operator()(Pred &&pred, Proj &&proj = {}, pos_t def = -1) const -> decltype(auto) {
+            MAP_TO_BASE(pred, proj, def);
         }
     };
 
     struct position_last_fn final : detail::algorithm_base {
         template <iterator I, sentinel S, std::invocable<iter_value_t<I>> Proj = meta::identity, std::predicate<std::invoke_result_t<Proj, iter_value_t<I>>> Pred>
-        constexpr auto operator()(I &&first, S &&last, Pred &&pred, Proj &&proj = {}) const -> std::size_t {
-            MAP_TO_IMPL(do_position_last, first, last, pred, proj);
+        constexpr auto operator()(I &&first, S &&last, Pred &&pred, Proj &&proj = {}, pos_t def = -1) const -> std::size_t {
+            MAP_TO_IMPL(do_position_last, first, last, pred, proj, def);
         }
 
         template <range Rng, std::invocable<range_value_t<Rng>> Proj = meta::identity, std::predicate<std::invoke_result_t<Proj, range_value_t<Rng>>> Pred>
-        constexpr auto operator()(Rng &&rng, Pred &&pred, Proj &&proj = {}) const -> std::size_t {
-            MAP_TO_IMPL(do_position_last, rng, pred, proj);
+        constexpr auto operator()(Rng &&rng, Pred &&pred, Proj &&proj = {}, pos_t def = -1) const -> std::size_t {
+            MAP_TO_IMPL(do_position_last, rng, pred, proj, def);
         }
 
         template <typename Pred, typename Proj = meta::identity>
-        constexpr auto operator()(Pred &&pred, Proj &&proj = {}) const -> decltype(auto) {
-            MAP_TO_BASE(pred, proj);
+        constexpr auto operator()(Pred &&pred, Proj &&proj = {}, pos_t def = -1) const -> decltype(auto) {
+            MAP_TO_BASE(pred, proj, def);
         }
     };
 
