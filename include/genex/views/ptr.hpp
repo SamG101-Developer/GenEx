e21d910db4a4bf09e43afc07f3c85a66579e713e
Sam Gardner <samuelgardner101@gmail.com>
2025-08-11 04:04:12 +0100

    Add a `get` for smart pointers

---
+++ include/genex/views/ptr.hpp
@@ -0,0 +1,47 @@
+#pragma once
+#include <coroutine>
+#include <functional>
+#include <genex/concepts.hpp>
+#include <genex/macros.hpp>
+#include <genex/meta.hpp>
+#include <genex/views/_view_base.hpp>
+
+using namespace genex::concepts;
+using namespace genex::type_traits;
+
+
+template <iterator I, sentinel S> requires (unique_ptr<deref_value_t<I>>)
+auto do_ptr_unique(I &&first, S &&last) -> genex::generator<typename deref_value_t<I>::pointer> {
+    for (; first != last; ++first) {
+        co_yield (*first).get();
+    }
+}
+
+
+template <range Rng> requires (unique_ptr<range_value_t<Rng>>)
+auto do_ptr_unique(Rng &&rng) -> genex::generator<typename range_value_t<Rng>::pointer> {
+    for (auto &&x : rng) {
+        co_yield x.get();
+    }
+}
+
+
+namespace genex::views {
+    struct ptr_unique_fn final : detail::view_base {
+        template <iterator I, sentinel S> requires (unique_ptr<deref_value_t<I>>)
+        constexpr auto operator()(I &&first, S &&last) const -> decltype(auto) {
+            MAP_TO_IMPL(do_ptr_unique, first, last);
+        }
+
+        template <range Rng> requires (unique_ptr<range_value_t<Rng>>)
+        constexpr auto operator()(Rng &&rng) const -> decltype(auto) {
+            MAP_TO_IMPL(do_ptr_unique, rng);
+        }
+
+        constexpr auto operator()() const -> decltype(auto) {
+            MAP_TO_BASE();
+        }
+    };
+
+    EXPORT_GENEX_STRUCT(ptr_unique);
+}
