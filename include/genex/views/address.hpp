819fad0547eb2c2c97ec8d5bb57ee61c0fa3f7f7
Sam Gardner <samuelgardner101@gmail.com>
2025-10-02 20:44:13 +0100

    New structure

--- include/genex/views/address.hpp
+++ include/genex/views/address.hpp
@@ -4,10 +4,12 @@
 #include <genex/generator.hpp>
 #include <genex/macros.hpp>
 #include <genex/pipe.hpp>
-#include <genex/iterators/iter_pair.hpp>
+#include <genex/iterators/access.hpp>
+#include <genex/operations/data.hpp>
+#include <genex/operations/size.hpp>
 
 
-namespace genex::views::concepts {
+namespace genex::views::detail::concepts {
     template <typename I, typename S>
     concept addressable_iters =
         std::input_iterator<I> and
@@ -24,39 +26,78 @@ namespace genex::views::concepts {
 
 
 namespace genex::views::detail {
-    template <typename I, typename S>
-        requires concepts::addressable_iters<I, S>
-    GENEX_NO_ASAN
-    auto do_address(I first, S last) -> generator<std::add_pointer_t<std::remove_reference_t<decltype(*std::declval<I&>())>>> {
-        if (first == last) { co_return; }
-        for (; first != last; ++first) {
-            co_yield std::addressof(*first);
+    template <typename I>
+    requires std::input_iterator<I> and std::indirectly_readable<I>
+    struct address_iterator {
+        using reference = std::add_pointer_t<std::iter_value_t<I>>;
+        using value_type = reference;
+        using pointer = reference;
+
+        GENEX_VIEW_ITERATOR_TYPE_DEFINITIONS(GENEX_ITER_CONCEPT_FOLLOW_THROUGH);
+        GENEX_VIEW_ITERATOR_CTOR_DEFINITIONS(address_iterator);
+        GENEX_VIEW_ITERATOR_FUNC_DEFINITIONS(address_iterator);
+
+        constexpr auto operator*() const noexcept
+            -> reference {
+            return std::addressof(*it);
         }
-    }
+
+        constexpr auto operator->() const noexcept
+            -> pointer {
+            return std::addressof(*it);
+        }
+    };
+
+    template <typename S>
+    struct address_sentinel {
+        GENEX_VIEW_SENTINEL_CTOR_DEFINITIONS(address_sentinel);
+        GENEX_VIEW_SENTINEL_FUNC_DEFINITIONS(address_iterator, address_sentinel);
+    };
+
+    template <typename I, typename S>
+    requires concepts::addressable_iters<I, S>
+    struct address_from_iters {
+        GENEX_VIEW_ITERSENT_CTOR_DEFINITIONS(address_from_iters);
+        GENEX_VIEW_ITERSENT_TYPE_DEFINITIONS(address_iterator, address_sentinel);
+        GENEX_VIEW_ITERSENT_FUNC_DEFINITIONS;
+    };
+
+    template <typename V>
+    requires (concepts::addressable_range<V>) //  and viewable_range<Rng>)
+    struct address_view : std::ranges::view_interface<address_view<V>> {
+        GENEX_VIEW_VIEW_CTOR_DEFINITIONS(address_view);
+        GENEX_VIEW_VIEW_TYPE_DEFINITIONS(address_iterator, address_sentinel);
+        GENEX_VIEW_VIEW_FUNC_DEFINITIONS;
+    };
 }
 
 
 namespace genex::views {
     struct address_fn {
         template <typename I, typename S>
-            requires concepts::addressable_iters<I, S>
-        constexpr auto operator()(I first, S last) const -> auto {
-            return detail::do_address(
-                std::move(first), std::move(last));
+        requires detail::concepts::addressable_iters<I, S>
+        constexpr auto operator()(I first, S last) const noexcept -> auto {
+            return detail::address_from_iters<I, S>{std::move(first), std::move(last)};
         }
 
         template <typename Rng>
-            requires concepts::addressable_range<Rng>
-        constexpr auto operator()(Rng &&rng) const -> auto {
-            auto [first, last] = iterators::iter_pair(rng);
-            return (*this)(
-                std::move(first), std::move(last));
+        requires (detail::concepts::addressable_range<Rng> and contiguous_range<Rng> and borrowed_range<Rng>)
+        constexpr auto operator()(Rng &&rng) const noexcept -> auto {
+            using V = std::views::all_t<Rng>;
+            return detail::address_view<V>{std::views::all(std::forward<Rng>(rng))}; // .as_pointer_subrange();
         }
 
-        auto operator()() const -> auto {
-            return std::bind_back(address_fn{});
+        template <typename Rng>
+        requires (detail::concepts::addressable_range<Rng>)
+        constexpr auto operator()(Rng &&rng) const noexcept -> auto {
+            using V = std::views::all_t<Rng>;
+            return detail::address_view<V>{std::views::all(std::forward<Rng>(rng))};
+        }
+
+        constexpr auto operator()() const noexcept -> auto {
+            return std::bind_front(address_fn{});
         }
     };
 
-    GENEX_EXPORT_STRUCT(address);
+    inline constexpr address_fn address{};
 }
--- include/genex/views/borrow.hpp
+++ include/genex/views/borrow.hpp
@@ -4,14 +4,16 @@
 #include <genex/generator.hpp>
 #include <genex/macros.hpp>
 #include <genex/pipe.hpp>
-#include <genex/iterators/iter_pair.hpp>
+#include <genex/iterators/access.hpp>
+#include <genex/operations/data.hpp>
+#include <genex/operations/size.hpp>
 
 
-namespace genex::views::concepts {
+namespace genex::views::detail::concepts {
     template <typename I, typename S>
     concept borrowable_iters =
         std::input_iterator<I> and
-        std::sentinel_for<S, I>;;
+        std::sentinel_for<S, I>;
 
     template <typename Rng>
     concept borrowable_range =
@@ -21,36 +23,80 @@ namespace genex::views::concepts {
 
 
 namespace genex::views::detail {
-    template <typename I, typename S>
-        requires concepts::borrowable_iters<I, S>
-    GENEX_NO_ASAN
-    auto do_borrow(I first, S last) -> generator<iter_value_t<I>&> {
-        if (first == last) { co_return; }
-        for (; first != last; ++first) {
-            co_yield *first;
+    template <typename I>
+    requires std::input_iterator<I> and std::indirectly_readable<I>
+    struct borrow_iterator {
+        using reference = iter_reference_t<I>;
+        using value_type = iter_value_t<I>;
+        using pointer = std::add_pointer_t<value_type>;
+
+        GENEX_VIEW_ITERATOR_TYPE_DEFINITIONS(GENEX_ITER_CONCEPT_FOLLOW_THROUGH);
+        GENEX_VIEW_ITERATOR_CTOR_DEFINITIONS(borrow_iterator);
+        GENEX_VIEW_ITERATOR_FUNC_DEFINITIONS(borrow_iterator);
+
+        constexpr auto operator*() const noexcept
+            -> reference {
+            return *it;
         }
-    }
+
+        constexpr auto operator->() const noexcept
+            -> pointer {
+            if constexpr (std::is_pointer_v<I>) {
+                return it;
+            } else {
+                return std::addressof(*it);
+            }
+        }
+    };
+
+    template <typename S>
+    struct borrow_sentinel {
+        GENEX_VIEW_SENTINEL_CTOR_DEFINITIONS(borrow_sentinel);
+        GENEX_VIEW_SENTINEL_FUNC_DEFINITIONS(borrow_iterator, borrow_sentinel);
+    };
+
+    template <typename I, typename S>
+    requires concepts::borrowable_iters<I, S>
+    struct borrow_from_iters {
+        GENEX_VIEW_ITERSENT_CTOR_DEFINITIONS(borrow_from_iters);
+        GENEX_VIEW_ITERSENT_TYPE_DEFINITIONS(borrow_iterator, borrow_sentinel);
+        GENEX_VIEW_ITERSENT_FUNC_DEFINITIONS;
+    };
+
+    template <typename V>
+    requires (concepts::borrowable_range<V>) //  and viewable_range<Rng>)
+    struct borrow_view : std::ranges::view_interface<borrow_view<V>> {
+        GENEX_VIEW_VIEW_CTOR_DEFINITIONS(borrow_view);
+        GENEX_VIEW_VIEW_TYPE_DEFINITIONS(borrow_iterator, borrow_sentinel);
+        GENEX_VIEW_VIEW_FUNC_DEFINITIONS;
+    };
 }
 
 
 namespace genex::views {
     struct borrow_fn {
         template <typename I, typename S>
-            requires concepts::borrowable_iters<I, S>
+        requires detail::concepts::borrowable_iters<I, S>
         constexpr auto operator()(I first, S last) const -> auto {
-            return detail::do_borrow(std::move(first), std::move(last));
+            return detail::borrow_from_iters<I, S>(std::move(first), std::move(last));
         }
 
         template <typename Rng>
-            requires concepts::borrowable_range<Rng>
+        requires detail::concepts::borrowable_range<Rng> and contiguous_range<Rng> and borrowed_range<Rng>
         constexpr auto operator()(Rng &&rng) const -> auto {
-            auto [first, last] = iterators::iter_pair(rng);
-            return (*this)(
-                std::move(first), std::move(last));
+            using V = std::views::all_t<Rng>;
+            return detail::borrow_view<V>{std::views::all(std::forward<Rng>(rng))}; // .as_pointer_subrange();
+        }
+
+        template <typename Rng>
+        requires detail::concepts::borrowable_range<Rng>
+        constexpr auto operator()(Rng &&rng) const -> auto {
+            using V = std::views::all_t<Rng>;
+            return detail::borrow_view<V>{std::views::all(std::forward<Rng>(rng))};
         }
 
         constexpr auto operator()() const -> auto {
-            return std::bind_back(borrow_fn{});
+            return std::bind_front(borrow_fn{});
         }
     };
 
