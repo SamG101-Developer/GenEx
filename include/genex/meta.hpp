627cb387183c8cb9a4a767dc8840e9e895d6d91c
Sam Gardner <samuelgardner101@gmail.com>
2025-10-08 21:53:17 +0100

    Use custom `invoke` (fixed pointer support)

--- include/genex/meta.hpp
+++ include/genex/meta.hpp
@@ -27,7 +27,7 @@ namespace genex::meta {
 
         /**
          * Member pointer invoke function that invokes a member function pointer on an object:
-         * obj.*mp(args...) or mp->*obj(args...).
+         * obj.*mp(args...).
          */
         template <typename T, typename C, typename Obj>
         GENEX_INLINE constexpr auto operator()(T C::*f, Obj &&obj) const noexcept(
@@ -37,14 +37,36 @@ namespace genex::meta {
         }
 
         /**
+         * Member pointer invoke function that invokes a member function pointer on an object:
+         * obj->*mp(args...).
+         */
+        template <typename T, typename C, typename Obj>
+        GENEX_INLINE constexpr auto operator()(T C::*f, Obj *obj) const noexcept(
+            noexcept(obj->*f))
+            -> decltype(obj->*f) {
+            return obj->*f;
+        }
+
+        /**
          * Member function pointer invoke function that invokes a member function pointer on an object with arguments:
-         * (obj.*mp)(args...) or (mp->*obj)(args...).
+         * (obj.*mp)(args...).
          */
         template <typename R, typename C, typename Obj, typename... Args>
         GENEX_INLINE constexpr auto operator()(R C::*f, Obj &&obj, Args &&... args) const noexcept(
             noexcept((std::forward<Obj>(obj).*f)(std::forward<Args>(args)...)))
             -> decltype((std::forward<Obj>(obj).*f)(std::forward<Args>(args)...)) {
             return (std::forward<Obj>(obj).*f)(std::forward<Args>(args)...);
+        }
+
+        /**
+         * Member function pointer invoke function that invokes a member function pointer on an object with arguments:
+         * (mp->*obj)(args...).
+         */
+        template <typename R, typename C, typename Obj, typename... Args>
+        GENEX_INLINE constexpr auto operator()(R C::*f, Obj *obj, Args &&... args) const noexcept(
+            noexcept((obj->*f)(std::forward<Args>(args)...)))
+            -> decltype((obj->*f)(std::forward<Args>(args)...)) {
+            return (obj->*f)(std::forward<Args>(args)...);
         }
     };
 
