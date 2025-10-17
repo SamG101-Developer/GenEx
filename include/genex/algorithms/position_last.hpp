ba10fc77446f49ed7a45ede25e8dea19d8d2ab43
Sam Gardner <samuelgardner101@gmail.com>
2025-10-17 05:30:42 +0100

    Minor bug fix

--- include/genex/algorithms/position_last.hpp
+++ include/genex/algorithms/position_last.hpp
@@ -26,7 +26,8 @@ namespace genex::algorithms::detail::impl {
     requires concepts::positionable_last_iters<I, S, Pred, Proj, Int> and std::bidirectional_iterator<I>
     GENEX_INLINE constexpr auto do_position_last(I first, S last, Pred &&pred, Proj &&proj, const Int def) -> Int {
         auto pos = static_cast<Int>(iterators::distance(first, last));
-        for (; first != last; --last, --pos) {
+        while (first != last) {
+            --last; --pos;
             if (meta::invoke(pred, meta::invoke(proj, *last))) { return pos; }
         }
         return def;
