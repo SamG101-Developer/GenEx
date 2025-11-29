c54593553af113523137209bfbcaf147bedf10f7
SamG101-Developer <samuelgardner101@gmail.com>
2025-11-29 16:33:37 +0000

    Const correctness

--- src/genex/algorithms/tuple.cpp
+++ src/genex/algorithms/tuple.cpp
@@ -85,12 +85,6 @@ namespace genex {
         }
 
         template <typename... Ts>
-        requires (N < sizeof...(Ts))
-        GENEX_INLINE constexpr auto operator()(std::tuple<Ts...> &&tpl) const -> decltype(auto) {
-            return std::get<N>(std::move(tpl));
-        }
-
-        template <typename... Ts>
         requires (N < sizeof...(Ts)) and (sizeof...(Ts) > 1)
         GENEX_INLINE constexpr auto operator()(Ts &&... args) const -> decltype(auto) {
             return std::get<N>(std::forward_as_tuple(std::forward<Ts>(args)...));
