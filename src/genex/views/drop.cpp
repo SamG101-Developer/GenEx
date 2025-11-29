67ca9140e786b84c9456057e0d84a80b7064ce1e
SamG101-Developer <samuelgardner101@gmail.com>
2025-11-29 13:17:23 +0000

    Modularization

--- src/genex/views/drop.cpp
+++ src/genex/views/drop.cpp
@@ -31,7 +31,7 @@ namespace genex::views::detail::impl {
     requires concepts::droppable_iters<I, S, Int> and std::random_access_iterator<I>
     auto do_drop(I first, S last, const Int n) -> generator<iter_value_t<I>> {
         GENEX_ITER_GUARD;
-        auto true_first = first + std::min<Int>(n, static_cast<Int>(iterators::distance(first, last)));
+        auto true_first = first + std::min<std::size_t>(static_cast<std::size_t>(n), iterators::distance(first, last));
         for (; true_first != last; ++true_first) {
             co_yield *true_first;
         }
@@ -52,20 +52,20 @@ namespace genex::views::detail::impl {
 namespace genex::views {
     struct drop_fn {
         template <typename I, typename S, typename Int>
-        requires detail::concepts::droppable_iters<I, S, Int>
+            requires detail::concepts::droppable_iters<I, S, Int>
         GENEX_INLINE auto operator()(I first, S last, const Int n) const {
             return detail::impl::do_drop(std::move(first), std::move(last), std::move(n));
         }
 
         template <typename Rng, typename Int>
-        requires detail::concepts::droppable_range<Rng, Int>
+            requires detail::concepts::droppable_range<Rng, Int>
         GENEX_INLINE auto operator()(Rng &&rng, const Int n) const {
             auto [first, last] = iterators::iter_pair(rng);
             return detail::impl::do_drop(std::move(first), std::move(last), std::move(n));
         }
 
         template <typename Int>
-        requires std::weakly_incrementable<Int>
+            requires std::weakly_incrementable<Int>
         GENEX_INLINE auto operator()(Int n) const {
             return meta::bind_back(drop_fn{}, std::move(n));
         }
