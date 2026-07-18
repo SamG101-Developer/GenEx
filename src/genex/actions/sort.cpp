9ec85ee716364e4e38ef5caa21f81543c4d82366
SamG101-Developer <samuelgardner101@gmail.com>
2026-07-18 13:07:56 +0100

    Add stable sort

--- src/genex/actions/sort.cpp
+++ src/genex/actions/sort.cpp
@@ -11,7 +11,7 @@ import std;
 
 namespace genex::actions::detail::concepts {
     template <typename Rng, typename Comp, typename Proj>
-    concept can_sorted_range =
+    concept can_sort_range =
         random_access_range<Rng> and
         std::sortable<iterator_t<Rng>, Comp, Proj>;
 }
@@ -19,7 +19,7 @@ namespace genex::actions::detail::concepts {
 namespace genex::actions {
     struct sort_fn {
         template <typename Rng, typename Comp = operations::lt, typename Proj = meta::identity>
-        requires detail::concepts::can_sorted_range<Rng, Comp, Proj>
+        requires detail::concepts::can_sort_range<Rng, Comp, Proj>
         GENEX_INLINE constexpr auto operator()(Rng &&rng, Comp comp = {}, Proj proj = {}) const -> decltype(auto) {
             auto [first, last] = iterators::iter_pair(rng);
             auto sorter = [&]<typename Lhs, typename Rhs>(Lhs &&lhs, Rhs &&rhs) {
@@ -36,5 +36,25 @@ namespace genex::actions {
         }
     };
 
+    struct stable_sort_fn {
+        template <typename Rng, typename Comp = operations::lt, typename Proj = meta::identity>
+        requires detail::concepts::can_sort_range<Rng, Comp, Proj>
+        GENEX_INLINE constexpr auto operator()(Rng &&rng, Comp comp = {}, Proj proj = {}) const -> decltype(auto) {
+            auto [first, last] = iterators::iter_pair(rng);
+            auto sorter = [&]<typename Lhs, typename Rhs>(Lhs &&lhs, Rhs &&rhs) {
+                return meta::invoke(comp, meta::invoke(proj, std::forward<Lhs>(lhs)), meta::invoke(proj, std::forward<Rhs>(rhs)));
+            };
+            std::stable_sort(std::move(first), std::move(last), std::move(sorter));
+            return std::forward<Rng>(rng);
+        }
+
+        template <typename Comp = operations::lt, typename Proj = meta::identity>
+        requires (not range<Comp>)
+        GENEX_INLINE constexpr auto operator()(Comp comp = {}, Proj proj = {}) const {
+            return meta::bind_back(sort_fn{}, std::move(comp), std::move(proj));
+        }
+    };
+
     export inline constexpr sort_fn sort{};
+    export inline constexpr stable_sort_fn stable_sort{};
 }
